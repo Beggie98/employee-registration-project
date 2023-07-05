86e4680bd87eddf1a8945af93c987936845eaac2
Beggie98 <bjuraev2020@gmail.com>
2023-07-05 13:22:06 +0500

    Initial commit

---
+++ src/main/java/dono/daryo/demo/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package dono.daryo.demo.controller;
+
+import dono.daryo.demo.bootstrap.DataGenerator;
+import dono.daryo.demo.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping("/register")
+    public String createEmployee(Model model){
+
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("states", DataGenerator.getAllStates());
+
+        return "/employee/employee-create";
+    }
+
+}
---
+++ src/test/java/dono/daryo/demo/EmployeeRegistrationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dono.daryo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistrationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
